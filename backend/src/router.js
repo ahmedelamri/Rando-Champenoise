6cefa3e19b9a03e045c17cc074ce1b537272fac1
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-05-11 15:21:50 +0200

    ajouts des randos

--- backend/src/router.js
+++ backend/src/router.js
@@ -11,7 +11,28 @@ const randos = [
     cityname: "Giffaumont-Champaubert",
     typeofcircuit: "discover circuits",
     distance: "7km",
+    picture:
+      "https://api.cloudly.space/resize/clip/1900/1080/75/aHR0cDovL21lZGlhcy50b3VyaXNtLXN5c3RlbS5jb20vZC82LzUyNzkyN19fcGJwMTMyOS5qcGc=/image.jpg",
+    dénivelé: "",
+  },
+  {
+    id: 2,
+    circuitname: "Circuit des Ajaux",
+    cityname: "Châlons-en-Champagne",
+    typeofcircuit: "viticultural circuits",
+    distance: "14km",
     picture: "",
+    dénivelé: "",
+  },
+  {
+    id: 3,
+    circuitname: "Circuit de la vallée de la Marne",
+    cityname: "Vauciennes",
+    typeofcircuit: "The heritage circuits",
+    distance: "30km",
+    picture:
+      "https://www.tourisme-en-champagne.com/sites/www.tourisme-en-champagne.com/files/styles/paragraphes_mosaique/public/thumbnails/image/statue-du-pape-urbain-ii-chatillon-sur-marnecm.-jolyot-coll.-adt-marne.jpg?itok=vyJS_PeB",
+    dénivelé: "",
   },
 ];
 
