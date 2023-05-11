ce0a9754cb5bac923b30036e46f4d3c42354fd98
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-05-11 14:50:17 +0200

    debut de l'api

--- backend/src/router.js
+++ backend/src/router.js
@@ -3,6 +3,16 @@ const express = require("express");
 const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
+const randos = [
+  {
+    id: 1,
+    circuitname: "Circuit des étangs d'Outines",
+    cityname: "Giffaumont-Champaubert",
+    typeofcircuit: "discover circuits",
+    distance: "7km",
+    picture: "",
+  },
+];
 
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
