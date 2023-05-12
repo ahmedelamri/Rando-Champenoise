424b1078181d284731915355b4ff9f4551e1de82
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-05-12 14:59:11 +0200

    Design modifié

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,15 @@
 import Logo from "../images/Rando-Champ.png";
 import Fonts from "../images/Fonts.png";
+import Gif from "../images/Foret.gif";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <img className="navbar-img" src={Logo} alt="Rando'Champenoise" />
-      <img className="nav" src={Fonts} alt="fonts" />
-    </nav>
+    <div>
+      <img className="gif" src={Gif} alt="Foret" />
+      <nav className="navbar">
+        <img className="navbar-img" src={Logo} alt="Rando'Champenoise" />
+        <img className="nav" src={Fonts} alt="fonts" />
+      </nav>
+    </div>
   );
 }
